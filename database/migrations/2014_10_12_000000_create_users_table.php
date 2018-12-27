5ff32e4d996e9ae78d06e44a3310760250c5f9a8
icoklo <igor.coklo@gmail.com>
2018-12-27 17:24:57 +0100

    created login and register pages

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->date('birthday')->nullable();
+
             $table->timestamps();
         });
     }
