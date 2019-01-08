6c18b7f186e271f301a5eca675a5c297b63fcd9c
icoklo <igor.coklo@gmail.com>
2019-01-08 16:45:40 +0100

    added jquery 3.3.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="app">
